7ba2dd7938fd354f58970783ca7a38ff88ece955
Gustavo Passos <96633763+TrXGustavoo@users.noreply.github.com>
2025-09-11 19:54:17 -0300

    Add files via upload

---
+++ pratica4/src/test/java/plataforma/pratica4/Pratica4ApplicationTests.java
@@ -0,0 +1,13 @@
+package plataforma.pratica4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Pratica4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
