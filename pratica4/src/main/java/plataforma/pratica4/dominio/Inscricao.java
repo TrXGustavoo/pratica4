fc6525aec7504344c8436fae879679abef0af66b
Gustavo Passos <96633763+TrXGustavoo@users.noreply.github.com>
2025-09-16 15:40:01 -0300

    Update Inscricao.java

--- pratica4/src/main/java/plataforma/pratica4/dominio/Inscricao.java
+++ pratica4/src/main/java/plataforma/pratica4/dominio/Inscricao.java
@@ -1,13 +1,13 @@
 package plataforma.pratica4.dominio;
 
-//Pacote: br.com.plataforma.dominio
+
 public class Inscricao {
 	private Curso curso;
-	private double progresso; // Em percentual, ex: 75.5
+	private double progresso; 
 
 	public Inscricao(Curso curso) {
 		this.curso = curso;
-		this.progresso = 0.0; // Inicia com 0%
+		this.progresso = 0.0; 
 	}
 
 	public Curso getCurso() {
@@ -21,4 +21,5 @@ public class Inscricao {
 	public void setProgresso(double progresso) {
 		this.progresso = progresso;
 	}
+
 }
