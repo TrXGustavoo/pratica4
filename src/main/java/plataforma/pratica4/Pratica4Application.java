6f06d9906c5b5afb4eb151db6c2d244e2f9d3819
Giovana Merguizo <giovana.pontes5654@hotmail.com>
2025-11-04 20:57:49 -0300

    Update Jenkinsfile.staging

--- src/main/java/plataforma/pratica4/Pratica4Application.java
+++ src/main/java/plataforma/pratica4/Pratica4Application.java
@@ -2,12 +2,23 @@ package plataforma.pratica4;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @SpringBootApplication
+@Controller
 public class Pratica4Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Pratica4Application.class, args);
+	}
+	
+	@RequestMapping ("/")
+	@ResponseBody
+	String home()
+	{
+	    return "Hello World";
 	}
 
 }
