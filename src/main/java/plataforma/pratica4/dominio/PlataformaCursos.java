a50263401a58d34f3d7e7f236ad12cf78b05359e
Giovana Merguizo <giovana.pontes5654@hotmail.com>
2025-11-06 20:22:02 -0300

    Comment out Optional import in PlataformaCursos
    Commented out the import statement for Optional.

--- src/main/java/plataforma/pratica4/dominio/PlataformaCursos.java
+++ src/main/java/plataforma/pratica4/dominio/PlataformaCursos.java
@@ -3,7 +3,7 @@ package plataforma.pratica4.dominio;
 import java.util.ArrayList;
 import java.util.stream.Collectors;
 import java.util.List;
-import java.util.Optional;
+//import java.util.Optional;
 
 public class PlataformaCursos {
 
