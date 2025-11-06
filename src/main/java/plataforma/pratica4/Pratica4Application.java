00edb693e8f76a03342b80e9f070c3a2a3c21647
GiMerguizo <giovana.pontes5654@hotmail.com>
2025-11-05 21:53:26 -0300

    update Application

--- src/main/java/plataforma/pratica4/Pratica4Application.java
+++ src/main/java/plataforma/pratica4/Pratica4Application.java
@@ -3,6 +3,8 @@ package plataforma.pratica4;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @SpringBootApplication
 @Controller
@@ -10,5 +12,12 @@ public class Pratica4Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Pratica4Application.class, args);
+	}
+
+	@RequestMapping ("/")
+		@ResponseBody
+		String home()
+		{
+			return "Hello World";
 	}
 }
